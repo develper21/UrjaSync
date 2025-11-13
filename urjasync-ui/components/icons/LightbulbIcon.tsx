11d9c6da52f905265e5ac00a554104aaf0dd2742
develper21 <githubnarvin@gmail.com>
2025-11-13 22:11:37 +0530

    add a icons LightblueIcon.tsx file

---
+++ urjasync-ui/components/icons/LightbulbIcon.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const LightbulbIcon = ({ className = 'w-6 h-6' }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className={className}
+  >
+    <path d="M15 14c.2-1 .7-1.7 1.5-2.5C17.7 10.2 18 9 18 8c0-2.2-1.8-4-4-4-3.1 0-4 3-4 3s-1-3-4-3C4 4 2 5.8 2 8c0 1 .3 2.2 1.5 3.5.8.8 1.3 1.5 1.5 2.5" />
+    <path d="M9 18h6" />
+    <path d="M10 22h4" />
+  </svg>
+);
+
+export default LightbulbIcon;
