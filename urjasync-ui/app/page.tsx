8d72dec23c08b7b0e0105e2cc4325a36088c09e7
develper21 <githubnarvin@gmail.com>
2025-11-13 22:13:24 +0530

    add a page.tsx file

---
+++ urjasync-ui/app/page.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import React, { useState } from 'react';
+import Sidebar from '@/components/layout/Sidebar';
+import Header from '@/components/layout/Header';
+import NotificationPopup from '@/components/layout/NotificationPopup';
+import DashboardView from '@/components/views/DashboardView';
+import AppliancesView from '@/components/views/AppliancesView';
+import OptimizationView from '@/components/views/OptimizationView';
+import RoutinesView from '@/components/views/RoutinesView';
+import BillingView from '@/components/views/BillingView';
+import SettingsView from '@/components/views/SettingsView';
+import { MOCK_DATA } from '@/lib/mockData';
+
+export default function Home() {
+  const [currentPage, setCurrentPage] = useState('dashboard');
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'dashboard':
+        return <DashboardView onNavigate={setCurrentPage} />;
+      case 'appliances':
+        return <AppliancesView />;
+      case 'optimization':
+        return <OptimizationView />;
+      case 'routines':
+        return <RoutinesView />;
+      case 'billing':
+        return <BillingView />;
+      case 'settings':
+        return <SettingsView />;
+      default:
+        return <DashboardView onNavigate={setCurrentPage} />;
+    }
+  };
+
+  return (
+    <div className="flex h-screen bg-gray-100 font-sans">
+      <Sidebar currentPage={currentPage} setCurrentPage={setCurrentPage} />
+
+      <div className="flex-1 flex flex-col overflow-hidden">
+        <Header peakStatus={MOCK_DATA.peakStatus} />
+        <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-50 p-8">
+          {renderPage()}
+        </main>
+      </div>
+
+      <NotificationPopup />
+    </div>
+  );
+}
