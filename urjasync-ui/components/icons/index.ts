65c73c92b293084d0aaef63664e27172597967d8
Narvin Sachaniya <githubnarvin@gmail.com>
2026-01-18 22:49:54 +0530

    Add icons/index.ts

---
+++ urjasync-ui/components/icons/index.ts
@@ -0,0 +1,14 @@
+export { MailIcon } from './MailIcon';
+export { LockIcon } from './LockIcon';
+export { EyeIcon } from './EyeIcon';
+export { EyeOffIcon } from './EyeOffIcon';
+export { PhoneIcon } from './PhoneIcon';
+export { GoogleIcon } from './GoogleIcon';
+export { AppleIcon } from './AppleIcon';
+
+// Re-export existing icons
+export { default as HomeIcon } from './HomeIcon';
+export { default as BellIcon } from './BellIcon';
+export { default as ZapIcon } from './ZapIcon';
+export { default as CogIcon } from './CogIcon';
+export { default as LogOutIcon } from './LogOutIcon';
