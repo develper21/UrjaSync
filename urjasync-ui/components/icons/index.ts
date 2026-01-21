f22dfdfe3a0a84fdfbe493dfdb9fb46af5f2b237
Narvin Sachaniya <githubnarvin@gmail.com>
2026-01-21 22:27:00 +0530

    Update icons index with new icon exports and improved organization

--- urjasync-ui/components/icons/index.ts
+++ urjasync-ui/components/icons/index.ts
@@ -12,3 +12,7 @@ export { default as BellIcon } from './BellIcon';
 export { default as ZapIcon } from './ZapIcon';
 export { default as CogIcon } from './CogIcon';
 export { default as LogOutIcon } from './LogOutIcon';
+export { default as ChartBarIcon } from './ChartBarIcon';
+export { default as ArrowsRightLeftIcon } from './ArrowsRightLeftIcon';
+export { default as LeafIcon } from './LeafIcon';
+export { default as WrenchIcon } from './WrenchIcon';
